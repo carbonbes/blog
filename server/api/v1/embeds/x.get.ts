80121dcabe78be8a6d5fbfcb33ba180f577987a2
Pavel <76450479+carbonbes@users.noreply.github.com>
2024-10-29 20:34:09 +0300

    improvements/embed-api

---
+++ server/api/v1/embeds/x.get.ts
@@ -0,0 +1,82 @@
+import { StorageMedia, Supabase } from '~/types'
+import { useSafeValidatedQuery, z } from 'h3-zod'
+import { getTweet } from 'react-tweet/api'
+
+async function getMedia(supabase: Supabase, url: string) {
+  const blob = await $fetch<any>(url)
+
+  if (!blob || !(blob instanceof Blob))
+    throw createError({
+      statusCode: 400,
+      message: 'Не удалось скачать медиафайл',
+    })
+
+  const buffer = Buffer.from(await blob.arrayBuffer())
+
+  return (await uploadFileToStorage({ supabase, file: buffer })) as StorageMedia
+}
+
+export default defineApiRoute(
+  async ({ event, supabase }) => {
+    const query = await useSafeValidatedQuery(event, { url: z.string().url() })
+
+    if (!query.success) {
+      throw createError({
+        statusCode: 400,
+        message: 'Укажите ID поста в X.com',
+      })
+    }
+
+    const { url } = query.data
+
+    const tweetIdRegexp =
+      /(?:https?:\/\/)?(?:www\.)?(?:twitter|x)\.com\/.+\/([0-9]{19})/gi
+
+    const [, postId] = tweetIdRegexp.exec(url) || []
+
+    const tweet = await getTweet(postId)
+
+    if (!tweet) {
+      throw createError({
+        statusCode: 404,
+        message: 'Пост не найден',
+      })
+    }
+
+    const authorAvatar = await getMedia(
+      supabase,
+      tweet.user.profile_image_url_https
+    )
+
+    let media: StorageMedia[] | undefined = undefined
+
+    if (tweet.mediaDetails) {
+      media = (
+        await Promise.all(
+          tweet.mediaDetails.map(async (tweetMedia) => {
+            return await getMedia(supabase, tweetMedia.media_url_https)
+          })
+        )
+      ).filter((media) => !!media)
+    }
+
+    const { name: authorName, screen_name: authorScreenName } = tweet.user
+
+    return {
+      author: {
+        avatar: authorAvatar,
+        name: authorName,
+        username: authorScreenName,
+        url: `https://x.com/${authorScreenName}`,
+      },
+      text:
+        tweet.text.replace(/https:\/\/t\.co\/\S+\s*$/gm, '').trim() ||
+        undefined,
+      media,
+      created_at: tweet.created_at,
+      type: 'x',
+      url,
+    }
+  },
+  { requireAuth: true }
+)
